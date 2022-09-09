7ac893ff60749bad229c018140ab4f6db71d238f
Egor <ee3068316@gmail.com>
2022-09-09 17:07:17 +0300

    add ex 3

---
+++ ex003/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Введите день недели и мы определим можно ли идти ту ту ту");
+int num_user = int.Parse(Console.ReadLine());
+if (num_user == 6 || num_user == 7)
+{
+    System.Console.WriteLine("Сегодня можно тутуту! ");
+}
+else if (num_user < 1 || num_user > 7)
+{
+    System.Console.WriteLine("Это не день недели, вы живете в матрице");
+}
+else 
+{
+    System.Console.WriteLine("Нельзя туту (");
+}
