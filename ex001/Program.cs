4c5109090e6e510932d1e40391700ec052a28750
Egor <ee3068316@gmail.com>
2022-09-09 11:47:56 +0300

    second num

--- ex001/Program.cs
+++ ex001/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите 3х значное число ");
+int num_user = int.Parse(Console.ReadLine());
+string stringNumber = Convert.ToString(num_user);
+Console.WriteLine("вторая цифра этого числа -> " +stringNumber[1]);
