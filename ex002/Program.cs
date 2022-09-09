51d446c267fc56b6ce8ede66800084e14f037e9d
Egor <ee3068316@gmail.com>
2022-09-09 12:00:30 +0300

    third num

---
+++ ex002/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите 3х значное число");
+int num_user = int.Parse(Console.ReadLine());
+string num_user_str = Convert.ToString(num_user);
+if (num_user_str.Length < 2)
+{
+    System.Console.WriteLine("У вашего числа нет 3ей цифры ");
+}
+else
+{
+    System.Console.WriteLine("3-е число ->" + num_user_str[2]);
+}
